aa3f541c840ed8a19dc668b9e791af4ffd9f6193
LvovNikita <lvov.nikita@yahoo.com>
2023-03-06 13:33:27 +0300

    Add creational patterns start code

---
+++ 01-creational/00-start-code.js
@@ -0,0 +1,78 @@
+const DIRECTION = {
+    NORTH: 0,
+    EAST: 1,
+    SOUTH: 2,
+    WEST: 3
+}
+
+class MapSite {
+    constructor() {}
+    enter() {}
+    
+}
+
+class Room extends MapSite {
+    constructor(roomNumber) {
+        super()
+        this.roomNumber = roomNumber
+        this.sides = new Array(4)
+    }
+    getSide(direction) {}
+    setSide(direction, mapSite) {
+        this.sides[direction] = mapSite
+    }
+}
+
+class Wall extends MapSite {
+    constructor() {
+        super()
+    }
+}
+
+class Door extends MapSite {
+    constructor(room1, room2) {
+        super()
+        this.isOpen = false
+        this.room1 = room1
+        this.room2 = room2
+    }
+    otherSideFrom(room) {
+
+    }
+}
+
+class Maze {
+    constructor() {
+        this.rooms = []
+    }
+    addRoom(room) {
+        this.rooms.push(room)
+    }
+    roomNo(roomNumber) {
+        // поиск комнаты по номеру
+    }  
+}
+
+function createMaze() {
+    const maze = new Maze()
+    const room1 = new Room(1)
+    const room2 = new Room(2)
+    const door = new Door(room1, room2)
+
+    maze.addRoom(room1)
+    maze.addRoom(room2)
+
+    room1.setSide(DIRECTION.NORTH, new Wall())
+    room1.setSide(DIRECTION.EAST, door)
+    room1.setSide(DIRECTION.SOUTH, new Wall())
+    room1.setSide(DIRECTION.WEST, new Wall())
+
+    room2.setSide(DIRECTION.NORTH, new Wall())
+    room2.setSide(DIRECTION.EAST, new Wall())
+    room2.setSide(DIRECTION.SOUTH, new Wall())
+    room2.setSide(DIRECTION.WEST, door)
+
+    console.dir(maze, { depth: 5 })
+}
+
+createMaze()
